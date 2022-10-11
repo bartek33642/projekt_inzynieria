7da04695852fb481b35409e494b0efa82abaf36c
broniuu <86781217+broniuu@users.noreply.github.com>
2022-10-11 21:44:45 +0200

    add api

---
+++ maxsat/src/main/java/com/example/maxsat/controller/GreetingController.java
@@ -0,0 +1,15 @@
+package com.example.maxsat.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting() {
+        return "Hello World!";
+    }
+
+}
---
+++ maxsat/src/test/java/com/example/maxsat/MaxsatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maxsat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaxsatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
