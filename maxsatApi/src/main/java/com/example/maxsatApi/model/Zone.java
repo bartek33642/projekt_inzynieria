5501dc52d46548b99117d527233e8cc0f7723ad9
broniuu <86781217+broniuu@users.noreply.github.com>
2022-10-19 16:21:31 +0200

    add hierarchy

---
+++ maxsatApi/src/main/java/com/example/maxsatApi/model/Zone.java
@@ -0,0 +1,10 @@
+package com.example.maxsatApi.model;
+
+public class Zone {
+    public int zoneId;
+    public int cordX;
+    public int cordY;
+    public double demandFactor;
+    public double accessibilityFactor;
+    public double attractivenessFactor;
+}
---
+++ maxsatApi/src/main/java/com/example/maxsatApi/service/ParkingLotService.java
@@ -0,0 +1,4 @@
+package com.example.maxsatApi.service;
+
+public class ParkingLotService {
+}
