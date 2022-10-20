35305f41fdc677446f6c48f8ca584bd412548ef7
Jakub Chochołowicz <j.chocholowicz@gmail.com>
2022-10-20 16:40:47 +0200

    add seedData

--- maxsatApi/src/main/java/com/example/maxsatApi/data/Seed.java
+++ maxsatApi/src/main/java/com/example/maxsatApi/data/Seed.java
@@ -6,7 +6,6 @@ import com.example.maxsatApi.service.ZoneService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-
 public class Seed {
 
     private  ZoneRepository zoneRepository;
@@ -17,8 +16,12 @@ public class Seed {
 
     public void seedData(){
         if (zoneRepository.count() == 0) {
-            Zone zone = new Zone(0, 0, 0.5, 0.25, 0.74);
-            zoneRepository.save(zone);
+            for (int x = 0; x < 10; ++x) {
+                for (int y = 0; y < 10; ++y) {
+                    Zone zone = new Zone(x, y, Math.round((Math.random() * (0.99 - 0.01) + 0.01) * 100.0) / 100.0, Math.round((Math.random() * (0.99 - 0.01) + 0.01) * 100.0) / 100.0, Math.round((Math.random() * (0.99 - 0.01) + 0.01) * 100.0) / 100.0);
+                    zoneRepository.save(zone);
+                }
+            }
         }
     }
 }
