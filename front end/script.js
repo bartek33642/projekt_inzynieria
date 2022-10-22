93339ceec93ac8186021e468a79e45b3ac896474
Szymon Grzeszczuk <szymon3520@gmail.com>
2022-10-22 14:51:02 +0200

    basic layout of page

--- front end/script.js
+++ front end/script.js
@@ -1,1 +1,21 @@
-let a=1;
+
+
+//setting up canvas element
+let mapCanvas = document.getElementById('mapCanvas').getContext('2d');
+
+let mapWidth = window.innerWidth * (40/100)
+let mapHeight = window.innerWidth * (25/100)
+
+mapCanvas.canvas.width  = mapWidth;
+mapCanvas.canvas.height = mapHeight;
+
+
+let backgroundCityImage = new Image(mapWidth, mapHeight)
+backgroundCityImage.src = "images/tarnów.png"
+
+backgroundCityImage.onload = () => 
+    mapCanvas.drawImage(backgroundCityImage, 0, 0)
+
+
+
+
