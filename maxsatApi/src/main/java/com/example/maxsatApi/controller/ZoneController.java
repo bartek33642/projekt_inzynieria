ec81532d15ad1ea74692e2bbade09940f56963ba
broniuu <86781217+broniuu@users.noreply.github.com>
2022-10-24 17:21:10 +0200

    add zone endpoint

--- maxsatApi/src/main/java/com/example/maxsatApi/controller/ZoneController.java
+++ maxsatApi/src/main/java/com/example/maxsatApi/controller/ZoneController.java
@@ -1,16 +1,28 @@
 package com.example.maxsatApi.controller;
 
 import com.example.maxsatApi.model.Zone;
+import com.example.maxsatApi.repository.ZoneRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 public class ZoneController {
+
+    @Autowired
+    public ZoneRepository zoneRepository;
     @GetMapping(value = "/zones")
     public Iterable<Zone> getZones(){
+        return zoneRepository.findAll();
+    }
 
-        return
+    @GetMapping(value = "/zones/{id}")
+    public Optional<Zone> getZoneById(@PathVariable Integer id){
+        return zoneRepository.findById(id);
     }
 }
