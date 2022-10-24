8589ac78a4b802a87ff182134675db89a0a19517
broniuu <86781217+broniuu@users.noreply.github.com>
2022-10-24 16:39:59 +0200

    Create ParkingLotRepository.java

---
+++ maxsatApi/src/main/java/com/example/maxsatApi/repository/ParkingLotRepository.java
@@ -0,0 +1,7 @@
+package com.example.maxsatApi.repository;
+
+import com.example.maxsatApi.model.ParkingLot;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ParkingLotRepository extends CrudRepository<ParkingLot, Integer> {
+}
