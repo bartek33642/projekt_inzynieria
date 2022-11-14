3669aa386513a9dc89f0a573c9f9ece29293c8f3
Szymon Grzeszczuk <szymon3520@gmail.com>
2022-11-14 13:03:52 +0100

    zones are displayed on canvas and zone id can be detected by click

--- front end/script.js
+++ front end/script.js
@@ -1,5 +1,6 @@
 //setting up canvas element
-let mapCanvas = document.getElementById('mapCanvas').getContext('2d');
+let canvasElement = document.getElementById('mapCanvas')
+let mapCanvas = canvasElement.getContext('2d');
 
 let mapWidth = window.innerWidth * (40/100)
 let mapHeight = window.innerWidth * (25/100)
@@ -7,63 +8,132 @@ let mapHeight = window.innerWidth * (25/100)
 mapCanvas.canvas.width  = mapWidth;
 mapCanvas.canvas.height = mapHeight;
 
-
 let backgroundCityImage = new Image(mapWidth, mapHeight)
 backgroundCityImage.src = "images/tarnów.png"
+//TODO: zmienić rozmiar mapy tarnowa w tle (nie wiem kurde jak to zrobić xd)
 
 backgroundCityImage.onload = () =>
     mapCanvas.drawImage(backgroundCityImage, 0, 0)
-
-// fetch('http://localhost:8081/zones', {
-//     method: 'GET',
-//     headers: {
-//         'Accept': 'application/json',
-//     },
-// })
-let headers = new Headers();
-headers.append('Access-Control-Allow-Origin', 'http://localhost:8081');
-headers.append('Access-Control-Allow-Credentials', 'true');
 
 const Http = new XMLHttpRequest();
 Http.open('GET', 'http://localhost:8081/zones');
 Http.send();
 
+let Zones;
+let listOfZonesCoordinates;
+
+//async function called when JSON is received
 Http.onreadystatechange = (e) => {
-    console.log(Http.responseText)
+    Zones = JSON.parse(Http.responseText);
+    listOfZonesCoordinates = getListOfZonesCoordinates(Zones);
+    drawNetOfHexagons(listOfZonesCoordinates);
 }
 
+canvasElement.addEventListener("click", (e) =>{
+    let {x,y} = getMousePositionFromCanvas(e);
+    let idOfClickedZone = getIdOfClickedZone(x, y, listOfZonesCoordinates);
+    console.log(idOfClickedZone);
+});
+//end of "main"
 
-drawHexagon = (posX, posY, size, borderColor = "#000000", isFilled = false, fillColor = "#ffffff") => {
+
+getMousePositionFromCanvas = (e) => {
+    let rect = canvasElement.getBoundingClientRect();
+    return {
+        x: e.clientX - rect.left,
+        y: e.clientY - rect.top
+    };
+}
+
+getIdOfClickedZone = (mouseX, mouseY, zonesCoords) => {
+    let bestCandidateId;
+    let bestCandidateDistance=9999;
+
+    for (let i = 0; i < zonesCoords.length; i++) {
+        const distance = Math.sqrt( (mouseX - zonesCoords[i].x) ** 2 + (mouseY - zonesCoords[i].y) ** 2 );
+        if (distance < bestCandidateDistance && distance <= zonesCoords[i].radius){
+            bestCandidateDistance = distance;
+            bestCandidateId = zonesCoords[i].zoneId;
+        }
+
+    }
+    return bestCandidateId;
+}
+
+getListOfZonesCoordinates = (zones) => {
+    let widthOfNet = 0, heightOfNet = 0;
+
+    for(let i = 0; i < zones.length; i++){
+        if( zones[i].cordY > heightOfNet)
+            heightOfNet = zones[i].cordY;
+        if( zones[i].cordX > widthOfNet)
+            widthOfNet = zones[i].cordX;
+    }
+
+    const margin = 0;
+
+    const x0 = 40;
+    const y0 = 40;
+
+    //todo: dać równe marginesy dla siatki hexagonów, tzn. żeby na dole była taka przerwa jak na górze
+
+    const a = (mapWidth - margin * widthOfNet - x0 * 2) / widthOfNet+1;
+    const b = (mapHeight - margin * heightOfNet - y0 * 2) / heightOfNet+1;
+    let c = a < b ? a : b;
+    const hexagonRadius = c/2 -1;
+
+    let listOfCoordinatesOfHexagons = new Array(zones.length);
+
+    for(let i = 0; i < zones.length; i++){
+        const cordX = zones[i].cordX;
+        const cordY = zones[i].cordY;
+
+        const oddRowAdjustment = cordX % 2 ? hexagonRadius * Math.sin( Math.PI / 3 ) : 0;
+
+        const x = (cordX * hexagonRadius) * (1 + Math.cos( Math.PI / 3 )) + x0 + margin * cordX;
+        const y = mapHeight - ( ( cordY * 2 * hexagonRadius) * Math.sin( Math.PI / 3 ) + oddRowAdjustment + y0 + margin * cordY);
+        const zoneId = zones[i].zoneId
+
+        listOfCoordinatesOfHexagons[i] = {
+            x: x,
+            y: y,
+            zoneId: zoneId,
+            radius: hexagonRadius
+        };
+    }
+    return listOfCoordinatesOfHexagons;
+}
+
+drawNetOfHexagons = (listOfCoordinates) =>{
+    for (let i = 0; i < listOfCoordinates.length; i++) {
+        drawHexagon( listOfCoordinates[i].x, listOfCoordinates[i].y, listOfCoordinates[i].radius );
+    }
+}
+
+drawHexagon = (posX, posY, radius, borderColor = "#000000", isFilled = false, fillColor = "#ffffff") => {
     let pi = Math.PI;
-    let r = size / 2;
 
-    mapCanvas.lineWidth = 5;
+    mapCanvas.lineWidth = 1;
 
     mapCanvas.fillStyle = fillColor;
     mapCanvas.strokeStyle  = borderColor;
 
     mapCanvas.beginPath();
-    mapCanvas.moveTo( posX + r * Math.cos( pi/3 ), posY + r * Math.sin( pi/3 ));
-    mapCanvas.lineTo( posX + r , posY);
-    mapCanvas.lineTo( posX + r * Math.cos( 5*pi/3 ), posY + r * Math.sin( 5*pi/3 ));
-    mapCanvas.lineTo( posX + r * Math.cos( 4*pi/3 ), posY + r * Math.sin( 4*pi/3 ));
-    mapCanvas.lineTo( posX - r , posY);
-    mapCanvas.lineTo( posX + r * Math.cos( 2*pi/3 ), posY + r * Math.sin( 2*pi/3 ));
-    mapCanvas.lineTo( posX + r * Math.cos( pi/3 ), posY + r * Math.sin( pi/3 ));
-    mapCanvas.lineTo( posX + r , posY);
-
-    isFilled ? mapCanvas.fill() : mapCanvas.stroke()
+    mapCanvas.moveTo( posX + radius * Math.cos( pi/3 ), posY + radius * Math.sin( pi/3 ));
+    mapCanvas.lineTo( posX + radius , posY);
+    mapCanvas.lineTo( posX + radius * Math.cos( 5*pi/3 ), posY + radius * Math.sin( 5*pi/3 ));
+    mapCanvas.lineTo( posX + radius * Math.cos( 4*pi/3 ), posY + radius * Math.sin( 4*pi/3 ));
+    mapCanvas.lineTo( posX - radius , posY);
+    mapCanvas.lineTo( posX + radius * Math.cos( 2*pi/3 ), posY + radius * Math.sin( 2*pi/3 ));
+    mapCanvas.lineTo( posX + radius * Math.cos( pi/3 ), posY + radius * Math.sin( pi/3 ));
+    mapCanvas.lineTo( posX + radius , posY);
 
     if(isFilled){
         mapCanvas.fill();
-        drawHexagon(posX, posY, size, borderColor, 0);
+        drawHexagon(posX, posY, radius, borderColor, 0);
     } else {
         mapCanvas.stroke();
     }
 }
-
-drawHexagon(100, 100, 150, '#007074', 1, '#ad1a1a');
-
-
 
 
