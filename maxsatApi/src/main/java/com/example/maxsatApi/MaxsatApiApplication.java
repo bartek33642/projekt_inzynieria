1b15091f1de16fc09e26b7cf4b469de4145014e8
broniuu <86781217+broniuu@users.noreply.github.com>
2022-11-17 18:44:03 +0100

    fix-cors

--- maxsatApi/src/main/java/com/example/maxsatApi/MaxsatApiApplication.java
+++ maxsatApi/src/main/java/com/example/maxsatApi/MaxsatApiApplication.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
@@ -25,5 +28,13 @@ public class MaxsatApiApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		new Seed(zoneRepository, parkingLotRepository).seedData();
+	}
+
+	@Bean public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
 	}
 }
--- maxsatApi/src/main/java/com/example/maxsatApi/controller/ZoneController.java
+++ maxsatApi/src/main/java/com/example/maxsatApi/controller/ZoneController.java
@@ -17,7 +17,7 @@ import java.util.Comparator;
 import java.util.List;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(maxAge = 3600)
 public class ZoneController {
 
     public final ZoneRepository zoneRepository;
